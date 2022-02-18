0c294d8f168e5eb5c07c8a7af490435e4f9aa92a
A SID <asidibe017@gmail.com>
2022-02-18 14:50:23 +0100

    ajout responsive

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,14 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelector(".close")
+const btnSubmit = document.querySelector(".btn-submit");
+const modalWindow = document.querySelector('.modal-body')
+const thankMessage = document.getElementById("thank")
+console.log(thankMessage);
+console.log(modalWindow);
+console.log(btnSubmit);
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +27,13 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+  close.addEventListener("click",function(){
+  modalbg.style.display = "none";
+  })
+
+  btnSubmit.addEventListener('click',function(){
+
+  })
 
 
