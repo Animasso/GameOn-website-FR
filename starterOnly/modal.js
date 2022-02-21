5839cca3f525792399eac3a45143bf1460723c0c
A SID <asidibe017@gmail.com>
2022-02-21 17:54:52 +0100

    mise en place des message erreur

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -15,6 +15,7 @@ const close = document.querySelector(".close")
 const btnSubmit = document.querySelector(".btn-submit");
 const modalWindow = document.getElementById('reserve')
 const thankMessage = document.getElementById("thank")
+
 const firstName = document.getElementById('first')
 const lastName = document.getElementById('last')
 const email = document.getElementById('email')
@@ -22,9 +23,32 @@ const birth = document.getElementById('bithdate')
 const radio = document.getElementById('quantity')
 const closeThanks = document.querySelector('.closeMgs')
 
-console.log(modalWindow);
-console.log(lastName);
-console.log(closeThanks);
+const firstError = document.getElementById('errorFirst');
+const lastError = document.getElementById('errorLast');
+const emailError = document.getElementById('errorEmail');
+const birthdateError = document.getElementById('errorBirth');
+const quantityError = document.getElementById('errorSelect');
+const locationsError = document.getElementById('errorRadio');
+const conditionError = document.getElementById('errorCondition');
+
+console.log(firstName);
+console.log(lastError);
+console.log(emailError);
+console.log(birthdateError);
+console.log(quantityError);
+console.log(locationsError);
+console.log(conditionError);
+
+
+
+
+
+
+
+
+
+
+
 
 
 // launch modal event
@@ -40,16 +64,36 @@ function launchModal() {
   })
 
   
-//faire apparaitre le message de remerciment apres submission
-  btnSubmit.addEventListener('click',(e)=>{
-    e.preventDefault()
-    
-    modalWindow.style.display ="none"
-    thankMessage.style.display ="block"
-  })
 
+
+
+
+
+
+modalWindow.addEventListener('submit',(e) => {
+    e.preventDefault();
+    let firstIsValid = false;
+    const firstNameValue = firstName.value;
+    console.log(firstNameValue);
+
+    if (firstNameValue != '' && firstNameValue.length >= 2) {
+      firstIsValid = true;
+    } else {
+      firstError.style.display = "block";
+    }
+
+
+  })
 
 //fermerture du message remerciment
 closeThanks.addEventListener('click',function () {
   modalbg.style.display = "none"
+})
+
+//faire apparaitre le message de remerciment apres submission
+btnSubmit.addEventListener('click',(e)=>{
+  e.preventDefault()
+  
+  modalWindow.style.display ="none"
+  thankMessage.style.display ="block"
 })
